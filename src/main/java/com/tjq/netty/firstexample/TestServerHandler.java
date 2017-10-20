c563b9dcd8a27a53c2237659707b45f42cc6d7ec
tang <jinquan.tang@ele.me>
2017-10-20 19:27:00 +0800

    more example

--- src/main/java/com/tjq/netty/firstexample/TestServerHandler.java
+++ src/main/java/com/tjq/netty/firstexample/TestServerHandler.java
@@ -7,6 +7,8 @@ import io.netty.channel.SimpleChannelInboundHandler;
 import io.netty.handler.codec.http.*;
 import io.netty.util.CharsetUtil;
 
+import java.net.URI;
+
 /**
  *
  * @Description: TODO
@@ -17,11 +19,53 @@ import io.netty.util.CharsetUtil;
 public class TestServerHandler extends SimpleChannelInboundHandler<HttpObject> {
     @Override
     protected void channelRead0(ChannelHandlerContext ctx, HttpObject msg) throws Exception {
+
+        System.out.println(msg.getClass());
+        System.out.println(ctx.channel().remoteAddress());
+        if(msg instanceof HttpRequest){
+        HttpRequest httpRequest =(HttpRequest)msg;
+        System.out.println("请求方法名" + (httpRequest.method().name()));
+        URI uri = new URI(httpRequest.uri());
+        if("/favicon.ico".equals(uri.getPath())){
+            System.out.println("请求favicon,ico");
+            return ;
+        }
         ByteBuf byteBuf = Unpooled.copiedBuffer("hi,netty", CharsetUtil.UTF_8);
         DefaultFullHttpResponse httpResponse = new DefaultFullHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.OK, byteBuf);
         httpResponse.headers().set(HttpHeaderNames.CONTENT_TYPE,"text/plain");
         httpResponse.headers().set(HttpHeaderNames.CONTENT_LENGTH,byteBuf.readableBytes());
         ctx.writeAndFlush(httpResponse);
+        }
 
+    }
+
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("channelActive");
+        super.channelActive(ctx);
+    }
+
+    @Override
+    public void channelRegistered(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("channelRegistered");
+        super.channelRegistered(ctx);
+    }
+
+    @Override
+    public void handlerAdded(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("handlerAdded");
+        super.handlerAdded(ctx);
+    }
+
+    @Override
+    public void channelInactive(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("channelInactive");
+        super.channelInactive(ctx);
+    }
+
+    @Override
+    public void channelUnregistered(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("channelUnregistered");
+        super.channelUnregistered(ctx);
     }
 }
